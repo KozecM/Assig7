07fb529b9e068ac08c474d945b204936570d323d
KozecM <kozecm@onid.oregonstate.edu>
2016-05-29 16:22:40 -0700

    maybe

--- public/js/vendor/insert.js
+++ public/js/vendor/insert.js
@@ -3,6 +3,11 @@ function Workout() {
 
 	this.insert = function(){
 		var woContent = document.getElementById("Name").value;
+		woContent += ", " +document.getElementById("Reps").value;
+		woContent += ", " +document.getElementById("Weight").value;
+		woContent += ", " +document.getElementById("Date").value;
+		woContent += ", " +document.getElementById("Lbs/Kg").value;
+
 		console.log(woContent);
 	}
 }
