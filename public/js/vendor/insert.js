56827b06ea7f5f74aae173ce9291cec545ce83e5
KozecM <kozecm@onid.oregonstate.edu>
2016-05-29 16:05:09 -0700

    s

--- public/js/vendor/insert.js
+++ public/js/vendor/insert.js
@@ -3,7 +3,7 @@ function Workout() {
 
 	this.insert = function(){
 		var woContent = document.getElementById("form_post");
-		console.log(woContent.getElementsByName('name').value);
+		console.log(woContent.getElementsByName("name").value);
 	}
 }
 
