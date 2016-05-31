ad7cee7d88e3127762d14bd594840e57f05efcfd
KozecM <kozecm@onid.oregonstate.edu>
2016-05-31 14:41:56 -0700

    fixing ajaxTable

--- public/js/vendor/insert.js
+++ public/js/vendor/insert.js
@@ -33,6 +33,7 @@ woContent.addEventListener("submit", function (e){
 		 	console.log("here ", response.workouts)
 		 	var id = response.workouts;
 
+		 	ajaxTable = [];
 		 	ajaxTable.push(id, name, reps, weight, date, lbs);
 
 		 	console.log(ajaxTable);
@@ -50,8 +51,8 @@ woContent.addEventListener("submit", function (e){
 
 		 	Cell[0].style.display = "none";
 
-		 	var updatebtn = '<input type="button" value="Update" onclick="updateRow(\x27worktable\x27,this,{{this.id}})">';
-		 	var deletebtn = '<input type="button" value="Delete" onclick="deleteRow(\x27worktable\x27,this,{{this.id}})"/>';
+		 	var updatebtn = '<input type="button" value="Update" onclick="updateRow(\x27ajxTbl\x27,this,{{this.id}})">';
+		 	var deletebtn = '<input type="button" value="Delete" onclick="deleteRow(\x27ajxTbl\x27,this,{{this.id}})"/>';
 
 		 	Cell.push(document.createElement('td'));
 		 	Cell[6].innerHTML = updatebtn;
