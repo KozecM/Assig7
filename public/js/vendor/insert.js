252b787d970a4b8735e99bcb3c7833a64e5fe9a2
KozecM <kozecm@onid.oregonstate.edu>
2016-06-01 13:07:07 -0700

    labEL

--- public/js/vendor/insert.js
+++ public/js/vendor/insert.js
@@ -153,7 +153,7 @@ function update(tableID, curRow, wID){
 
 		if(mainRow == curRow.parentNode.parentNode){
 
-			console.log(table.rows[i].cells[1].lable);
+			console.log(table.rows[i].cells[1].label);
 			name=table.rows[i].cells[1].value;
 			reps =table.rows[i].cells[2].Reps;
 			weight =table.rows[i].cells[3].Weight.value;
