70163b29c4ef7fa2952b157aa6121e30fd529101
KozecM <kozecm@onid.oregonstate.edu>
2016-06-01 13:35:04 -0700

    maybe?

--- public/js/vendor/insert.js
+++ public/js/vendor/insert.js
@@ -153,7 +153,7 @@ function update(tableID, curRow, wID){
 
 		if(mainRow == curRow.parentNode.parentNode){
 
-			console.log(document.getElementsByName(Name)[1].value);
+			console.log(document.querySelector(#ajaxupdate > td:nth-child(2) > label > input).value);
 			name=table.rows[i].cells[1];
 			name = name.Name;
 			console.log(name);
