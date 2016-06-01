ffa84563209064292e1bebb3c7d62ac940410288
KozecM <kozecm@onid.oregonstate.edu>
2016-05-31 22:27:34 -0700

    fixing form

--- public/js/vendor/insert.js
+++ public/js/vendor/insert.js
@@ -61,9 +61,7 @@ woContent.addEventListener("submit", function (e){
 		 	theform.id = "ajaxupdate";
 		 	theform.display = 'none';
 
-		 	var formcell = document.createElement('td');
-		 	formcell.style.display = 'none';
-		 	formcell.innerHTML = "<form>\
+		 	theform.innerHTML = "<form>\
 			<label>Name:\
 				<input type=\x27text\x27 name=\x27Name\x27 id=\x27Name\x27 value=\x27" + name +"\x27>\
 			</label>\
